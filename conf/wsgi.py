cc629e01c247f929aec7f2299f269fac395b839f
daniel ivan <admin@example.com>
2021-02-18 18:20:12 -0300

    segunda version para produccion

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
-os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings.local"
-# os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings.production"
+# os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings.local"
+os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings.production"
 
 application = get_wsgi_application()
